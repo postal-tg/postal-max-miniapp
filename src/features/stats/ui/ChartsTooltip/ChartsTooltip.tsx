795bae9191a9989e9efa4d324a1107017a26921f
tgafiulin <t.gafiulin@ya.ru>
2026-01-14 13:47:11 +0300

    Удаление времени в чарте Охватов

--- src/features/stats/ui/ChartsTooltip/ChartsTooltip.tsx
+++ src/features/stats/ui/ChartsTooltip/ChartsTooltip.tsx
@@ -22,7 +22,6 @@ export function ChartsTooltip({ active, label, payload }: ChartsTooltipProps) {
     <div className="tooltip">
       <div className="tooltip-row">
         <div className="tooltip-date">{formatRuWeekdayShortDate(label as string)}</div>
-        <div className="tooltip-time">{formatTimeHHmm(label as string)}</div>
       </div>
       <div className="tooltip-row">
         <span className="tooltip-label">Охват</span>
