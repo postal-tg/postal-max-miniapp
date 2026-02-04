5202d5884a2cf134ca1e27170fbcb2d0de3d0a6d
tgafiulin <t.gafiulin@ya.ru>
2026-02-04 14:23:38 +0300

    Обновление .env

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from "node:url";
 import { resolve, dirname  } from "path";
@@ -7,19 +7,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4000,           // Фиксированный порт
-    strictPort: true,     // Ошибка, если порт занят (не переключится на другой)
-    host: true,           // Для ngrok: доступен извне (--host)
-    allowedHosts: [
-      'jannette-grimy-shiela.ngrok-free.dev' // твой ngrok-домен
-    ]    
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, resolve(__dirname), '');
+  
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [react()],
+    server: {
+      port: 4000,           // Фиксированный порт
+      strictPort: true,     // Ошибка, если порт занят (не переключится на другой)
+      host: true,           // Для ngrok: доступен извне (--host)
+      allowedHosts: env.NGROK_HOST 
+        ? [env.NGROK_HOST]
+        : []
     },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    }
   }
 })
